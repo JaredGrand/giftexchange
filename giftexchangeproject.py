9131321868fc7a033c8d54e2fd55405e17f0d52d
Jared Grandlienard <jared.a.grandlienard@gmail.com>
2018-11-27 20:37:01 -0600

    Initialized a dictionary to store who draws who

--- giftexchangeproject.py
+++ giftexchangeproject.py
@@ -5,3 +5,10 @@ import random
 def people_list():
     str1 = "Please list the names of the participants separated by commas: "
     return input(str1)
+
+list1 = people_list()
+list1 = list1.split(',')
+
+exchange = {}
+for name in list1:
+    exchange[name] = random.choice(list1)
