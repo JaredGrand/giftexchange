e841faf342734123cedfa1ddcfefdfa5defd0aad
Jared Grandlienard <jared.a.grandlienard@gmail.com>
2018-11-27 20:23:27 -0600

    Added people_list function to define participants

--- giftexchangeproject.py
+++ giftexchangeproject.py
@@ -1,3 +1,7 @@
 """This program is designed to take a list of names and randomize who gives presents to who in a gift exchange"""
 
 import random
+
+def people_list():
+    str1 = "Please list the names of the participants separated by commas: "
+    return input(str1)
